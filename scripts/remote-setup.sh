5cfdd6ba181235d288bbc1c0b646ad92e43a51f0
Jorg8383 <joerg.striebel@icloud.com>
2026-05-24 13:57:30 +0000

    fix - app window wasn't shown in fullscreen mode on the target

--- scripts/remote-setup.sh
+++ scripts/remote-setup.sh
@@ -54,7 +54,7 @@ cat > "${APP_REMOTE_DIR}/run-app.sh" <<EOF
 #!/usr/bin/env bash
 set -euo pipefail
 export LD_LIBRARY_PATH="${QT_INSTALL_DIR}/lib:\${LD_LIBRARY_PATH:-}"
-exec "${APP_REMOTE_DIR}/${APP_BINARY_NAME}"
+exec "${APP_REMOTE_DIR}/${APP_BINARY_NAME}" --embedded
 EOF
 
 chmod +x "${APP_REMOTE_DIR}/run-app.sh"
