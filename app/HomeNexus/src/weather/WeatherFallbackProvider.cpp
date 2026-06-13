a7f7acd75e1198ebffa6714f1cbb756e276c88ef
Jörg Striebel <92162900+Jorg8383@users.noreply.github.com>
2026-06-13 11:46:37 +0200

    add weather fall back provider

---
+++ app/HomeNexus/src/weather/WeatherFallbackProvider.cpp
@@ -0,0 +1,72 @@
+#include "WeatherFallbackProvider.hpp"
+#include <QFile>
+#include <QStringLiteral>
+#include <QByteArray>
+#include "OpenWeatherParser.hpp"
+
+WeatherFallbackProvider::WeatherFallbackProvider(QObject *parent)
+    : QObject{parent}
+{}
+
+void WeatherFallbackProvider::loadData(const QString &weatherFilePath, const QString &forecastFilePath)
+{
+    QString errorMessage;
+
+    QByteArray weatherRawData;
+    if (!readJsonFile(weatherFilePath, weatherRawData, errorMessage))
+    {
+        emit errorOccurred(errorMessage);
+        return;
+    }
+
+    WeatherData weatherData;
+    if (!OpenWeatherParser::parseWeather(weatherRawData, weatherData))
+    {
+        emit errorOccurred(QStringLiteral("Failed to parse JSON file: %1").arg(weatherFilePath));
+        return;
+    }
+
+    QByteArray forecastRawData;
+    if (!readJsonFile(forecastFilePath, forecastRawData, errorMessage))
+    {
+        emit errorOccurred(errorMessage);
+        return;
+    }
+
+    ForecastData forecastData;
+    if (!OpenWeatherParser::parseForecast(forecastRawData, forecastData))
+    {
+        emit errorOccurred(QStringLiteral("Failed to parse JSON file: %1").arg(forecastFilePath));
+        return;
+    }
+
+    emit weatherLoaded(weatherData);
+    emit forecastLoaded(forecastData);
+}
+
+bool WeatherFallbackProvider::readJsonFile(const QString &filePath, QByteArray &json, QString &errorMessage)
+{
+    QFile file(filePath);
+
+    if (!file.exists())
+    {
+        errorMessage = QStringLiteral("Fallback JSON file does not exist: %1").arg(filePath);
+        return false;
+    }
+
+    if (!file.open(QIODeviceBase::ReadOnly))
+    {
+        errorMessage = QStringLiteral("Could not open fallback JSON file: %1").arg(filePath);
+        return false;
+    }
+
+    json = file.readAll();
+
+    if (json.isEmpty())
+    {
+        errorMessage = QStringLiteral("Fallback JSON file is emtpy: %1").arg(filePath);
+        return false;
+    }
+
+    return true;
+}
---
+++ app/HomeNexus/src/weather/WeatherFallbackProvider.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <QObject>
+#include <QString>
+#include <QByteArray>
+
+#include "WeatherData.hpp"
+#include "ForecastData.hpp"
+
+class WeatherFallbackProvider : public QObject
+{
+    Q_OBJECT
+public:
+    explicit WeatherFallbackProvider(QObject *parent = nullptr);
+
+    void loadData(const QString &weatherFilePath, const QString &forecastFilePath);
+
+private:
+    bool readJsonFile(const QString &filePath, QByteArray &json, QString &errorMessage);
+
+signals:
+    void weatherLoaded(const WeatherData &weatherData);
+    void forecastLoaded(const ForecastData &forecastData);
+    void errorOccurred(const QString &errorMessage);
+
+};
